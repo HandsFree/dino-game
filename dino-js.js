b169e2400819dd35307cbd4bbd6f48a2652a004e
Jim1111 <jmflint1@gmail.com>
2021-05-14 18:06:34 +0100

    Update dino-js.js
    Added dinosaur image and text.

--- dino-js.js
+++ dino-js.js
@@ -1,10 +1,9 @@
 
-/*
+
 var dino = {
     x: 50,
-    y: 300,
+    y: 100
 }
-*/
 
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
@@ -14,11 +13,19 @@ document.body.appendChild(canvas);
 canvas.width = 1200;
 canvas.height = 700;
 
-/*
+
 mainImage = new Image();
 mainImage.ready = false;
 mainImage.onload = checkReady;
 mainImage.src = "RunSpritesSh1.png";
+
+var keyclick = {};
+
+document.addEventListener("keydown", function (event) {
+    keyclick[event.keyCode]=true;
+    console.log(keyclick);
+}, false);
+
 
 
 function checkReady() {
@@ -35,19 +42,13 @@ function playgame() {
 function render() {
     context.fillStyle="green";
     context.fillRect(0,0,canvas.width,canvas.height);
+    context.drawImage(mainImage,0,0,150,160,dino.x,dino.y,120,80);
+    context.drawImage(mainImage,150,0,150,160,400,200,120,80);
+    context.drawImage(mainImage,150,0,150,160,700,500,120,80);
+    context.font = "20px Verdana";
+    context.fillStyle = "black";
+    context.fillText("Directions: arrow keys to move", 400, 690);
 }
-
-*/
-
-/*comment this out*/
-context.fillStyle="green";
-context.fillRect(0,0,canvas.width,canvas.height);
-
-
-context.font = "20px Verdana";
-context.fillStyle = "black";
-context.fillText("Directions: arrow keys to move", 400, 690);
-
 
     
 
